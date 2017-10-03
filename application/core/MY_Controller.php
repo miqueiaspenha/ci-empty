aa9bc7002b38aa6c44a60fe89dc0a017c2806000
Miqueias Penha <miqueiaspenha@gmail.com>
2017-10-03 12:27:09 -0300

    adicionado REST_Controller e removido o .env e adiconado o example

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,3 +9,32 @@ class MY_Controller extends CI_Controller
         parent::__construct();
     }
 }
+
+class REST_Controller extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        header('Accept: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: Content-Type, X-Authorization, Authorization');
+        header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+        header('Content-Type: application/json');
+    }
+
+    public function get_data($array = false)
+    {
+        return json_decode(file_get_contents('php://input'), $array);
+    }
+
+    public function write_output($message = [], $http_status = 400)
+    {
+        $this->output->set_status_header($http_status);
+        return $this->output->set_output(json_encode($message));
+    }
+
+    public function generate_token($data = '')
+    {
+        return JWT::encode($data, getenv('SECRET_KEY'));
+    }
+}
