db1bb2de6f0a4bc8550038300351d8aaa5a6d2e0
Miqueias Penha <miqueiaspenha@gmail.com>
2018-05-03 17:39:05 -0300

    adicinado o arquivo que faz regras gustomizadas

---
+++ application/libraries/MY_Form_validation.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class MY_Form_validation extends CI_Form_validation
+{
+
+    protected $CI;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->CI =& get_instance();
+    }
+
+    public function rule_example($value)
+    {
+        $variable = false;
+        if ($variable) {
+            $this->set_message('rule_example', 'message');
+            return false;
+        }
+        return true;
+    }
+}
