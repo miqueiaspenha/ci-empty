db06b88697ede6e15304e78b757130ebbe0923b5
Miqueias Penha <miqueiaspenha@gmail.com>
2018-11-26 16:46:49 -0300

    Update Migrate.php
    add funções para fazer validações na variável passada

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -3,21 +3,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Migrate extends CI_Controller
 {
-    public function index($version = '')
+    public function index($version = -1)
     {
         if (!$this->input->is_cli_request()) {
-            echo '<h1>Acesso restrito ao console!</h1>';
+            echo '<h1>Acesso restrito ao console!</h1>' . PHP_EOL;
             return;
         }
-
+        if ($version < -1) {
+            echo 'Valor passado é inválido' . PHP_EOL;
+            return;
+        }
         $this->load->library('migration');
-
-        $retorno = ($version) ? $this->migration->version($version) : $this->migration->current();
-
-        if ($retorno) {
+        $retorno = ((int) $version) > -1 ? $this->migration->version($version) : $this->migration->current();
+        if ($retorno === false) {
             show_error($this->migration->error_string());
             return;
         }
-        echo 'Migration completed.';
+        echo 'Migration completed.' . PHP_EOL;
     }
 }
